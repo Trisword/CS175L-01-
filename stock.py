6cef184cbf485c779c8b18b2580c48d44d9e2088
Trisword <olalekan2nd@gmail.com>
2023-01-30 08:02:54 -0500

    Add files via upload

--- stock.py
+++ stock.py
@@ -1,15 +1,18 @@
-stock1 = 2000
-pps = 40
-totalprice1 = pps * stock1
-comission1 = totalprice1 * 0.03
-stock2 = 2000
-pps2 = 42.75
-totalprice2 = pps2 * stock2
-comission2 = totalprice2 * 0.03
-profit = totalprice2 - (totalprice1 + comission1 + comission2)
+##Azeez Olapade
+##CS175L
+##stocks
 
-print(f"Amount paid for the stock: ${totalprice1:.2f}")
-print(f"Comission paid on the purchase: ${comission1:.2f}")
-print(f"Amount the stock sold for: ${totalprice2:.2f}")
-print(f"Comission paid on the sale: ${comission2:.2f}")
-print(f"Profit (or loss if negative): ${profit:.2f}")
+NUM_SHARES = 2000
+PURCHASE_PRICE = 40
+amountPaidForStock = PURCHASE_PRICE * NUM_SHARES
+purchaseCommission = amountPaidForStock * 0.03
+SELLING_PRICE = 42.75
+stockSoldFor = SELLING_PRICE * NUM_SHARES
+sellingCommission = stockSoldFor * 0.03
+profitOrLoss = stockSoldFor - (amountPaidForStock + purchaseCommission + sellingCommission)
+
+print(f"Amount paid for the stock: ${PURCHASE_PRICE:.2f}")
+print(f"Commission paid on the purchase: ${purchaseCommission:.2f}")
+print(f"Amount the stock sold for: ${SELLING_PRICE:.2f}")
+print(f"Commission paid on the sale: ${sellingCommission:.2f}")
+print(f"Profit (or loss if negative): ${profitOrLoss:.2f}")
