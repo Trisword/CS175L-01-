8b47e605db884eca2332e5f1d6ef971cad635d32
Trisword <olalekan2nd@gmail.com>
2023-03-06 13:10:48 -0500

    Add files via upload

--- averagingfile.py
+++ averagingfile.py
@@ -1,15 +1,21 @@
 def main():
-    outfile = open('numbers.txt', 'r')
-
     total = 0
     num = 0
+
+    
+    outfile = open('numbers.txt', 'r')
     for line in outfile:
         total = total + float(line)
         num+=1
+        print(f"I read in {num:.2f} numbers(s)  Current number is: {float(line):.2f}")
+        print(f"Total is: {total:.2f}")
+
 
     average = total / num
-    print(average)    
     outfile.close()
+
+    print("Average is:", average)
+
 if __name__ == '__main__':
     main()
         
