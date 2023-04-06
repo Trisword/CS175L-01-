5790e2614ddf2061d46967e9ecab5127cb94accb
Trisword <olalekan2nd@gmail.com>
2023-04-06 12:23:21 -0400

    Add files via upload

--- worldseries.py
+++ worldseries.py
@@ -7,11 +7,20 @@ def main():
     while True:
         count = 0
         user_input = input("Enter the name of the team or quit: ")
-        if user_input == 'q' or 'quit':
+        if user_input == quit or user_input == 'q':
             quit()
+            
+        while user_input is not any(lines):
+            print("You searched an invalid team name, please try again")
+            user_input = input("Enter the name of the team or quit: ")
+            if user_input == quit or user_input == 'q':
+                quit()
+            
         for x in lines:
-            if user_input.casefold() in x.casefold():
+            if user_input.casefold() == x.casefold():
                 count+=1
+        
+        
         print(f'The {user_input} won the world series {count} times between 1903 and 2009')
         
 
